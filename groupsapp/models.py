c978d0d802099b1d7ffb178356fa503ba550ef51
Josh <joshdavidson0510@gmail.com>
2020-01-23 21:28:49 +0000

    Added UserGroupLink model

--- groupsapp/models.py
+++ groupsapp/models.py
@@ -30,6 +30,11 @@ class UserMemberLink(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     member = models.ForeignKey(Member,on_delete=models.CASCADE)
 
+class UserGroupLink(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    group = models.ForeignKey(Group,on_delete=models.CASCADE)
+    role = models.CharField(max_length=20)
+
 class MemberGroupLink(models.Model):
     member = models.ForeignKey(Member,on_delete=models.CASCADE)
     group = models.ForeignKey(Group,on_delete=models.CASCADE)
